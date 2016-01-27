5391ad7a572d5605c000f4652f8d9cfb09a71ddf
wchb2015 <1021094673@qq.com>
2016-01-27 18:57:52 +0800

    springmvc!

--- src/main/java/com/springmvc/action/UserController.java
+++ src/main/java/com/springmvc/action/UserController.java
@@ -3,6 +3,7 @@ package com.springmvc.action;
 
 import com.springmvc.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -18,28 +19,31 @@ import java.util.List;
 
 
 @org.springframework.stereotype.Controller
-@RequestMapping("/myController")
-public class MyController implements Controller {
+@RequestMapping("/user-module")
+public class UserController {
 
 
-    @RequestMapping("1.html")
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
+    public String getUsers(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         List<User> userList = new ArrayList<User>();
+
         User u1 = new User("aaa", "123", 1);
         User u2 = new User("bbb", "123", 1);
         User u3 = new User("ccc", "123", 1);
         userList.add(u1);
         userList.add(u2);
         userList.add(u3);
-        ModelAndView modelAndView = new ModelAndView();
+
+        request.setAttribute("userList", userList);
+
+        return "userList";
+
+       /* ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.addObject("userList", userList);//相当于request的setAttribute,在jsp页面中通过userList取数据
 
-
-        modelAndView.setViewName("/WEB-INF/pages/userList.jsp"); //指定视图
-
-        return modelAndView;
+        modelAndView.setViewName("userList"); //指定视图*/
 
     }
 }
--- src/main/webapp/WEB-INF/pages/userList.jsp
+++ src/main/webapp/WEB-INF/pages/userList.jsp
@@ -6,11 +6,30 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
+<h2>All Users in System</h2>
 
+<table border="1">
+    <tr>
+        <th>username</th>
+        <th>passwd</th>
+        <th>age</th>
+        <th>createTime</th>
+    </tr>
+    <c:forEach items="${userList}" var="user">
+        <tr>
+            <td>${user.username}</td>
+            <td>${user.passwd}</td>
+            <td>${user.age}</td>
+            <td>${user.createTime}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
